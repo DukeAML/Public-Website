2e646e8f2ff8ab2ede37be066f827c733233e59b
NICOLE MOISEYEV <nicole.moiseyev3400@gmail.com>
2019-10-19 20:09:18 -0400

    Make homepage nav links

--- src/components/homepage/HomePage.js
+++ src/components/homepage/HomePage.js
@@ -16,7 +16,7 @@ import Animation from "../canvas/Animation";
 import Footer from "../tools/Footer";
 import ThemeCard from "./ThemeCard";
 
-import "./HomePage.css"
+import "./HomePage.scss";
 
 class HomePage extends React.Component {
   state = { redirect: false };
@@ -38,47 +38,109 @@ class HomePage extends React.Component {
 
     return (
       <div>
-        <Container style={{ height: "100%", paddingTop:"60px" }} />
+        <Container style={{ height: "100%", paddingTop: "60px" }} />
 
-          {/**INTRODUCTION SECTION */}
-          <Container fluid style = {{marginBottom:"10px"}}>
-            <Row className="justify-content-md-center" style ={{fontSize:"20px"}}>
-              {/**Navigation Area */}
-              <Col md = {2} sm = {4} xs = {4} style= {{textAlign:"center"}}>
-                <Image src = {require("./images/DAML-logo-square.svg")} style ={{maxHeight:"40px"}} />
-              </Col>
-              <Col md = {2} sm = {4} xs = {4} style= {{display:"flex", justifyContent: 'center', alignItems: 'center'}}>
+        {/**INTRODUCTION SECTION */}
+        <Container fluid style={{ marginBottom: "10px" }}>
+          <Row
+            className="justify-content-md-center"
+            style={{ fontSize: "20px" }}
+          >
+            {/**Navigation Area */}
+            <Col md={2} sm={4} xs={4} style={{ textAlign: "center" }}>
+              <Link to="/">
+                <Image
+                  src={require("./images/DAML-logo-square.svg")}
+                  style={{ maxHeight: "40px" }}
+                />
+              </Link>
+            </Col>
+            <Col
+              md={2}
+              sm={4}
+              xs={4}
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center"
+              }}
+              to="/projects"
+            >
+              <Link to="/projects" className="homepage-link">
                 Projects
-              </Col>
-              <Col md = {2}  sm = {4} xs = {4} style= {{display:"flex", justifyContent: 'center', alignItems: 'center'}}>
+              </Link>
+            </Col>
+            <Col
+              md={2}
+              sm={4}
+              xs={4}
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center"
+              }}
+            >
+              <Link to="/people" className="homepage-link">
                 People
-              </Col>
-            </Row>
-            <Row style= {{display:"flex", justifyContent: 'center', alignItems: 'center', paddingTop:"65px"}}>
-              {/**Icon */}
-                <Image src = {require("./images/DAML-logo.svg")}/>
-            </Row>
-          </Container>
+              </Link>
+            </Col>
+          </Row>
+          <Row
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              paddingTop: "65px"
+            }}
+          >
+            {/**Icon */}
+            <Image
+              className="main-graphic"
+              src={require("./images/DAML-logo.svg")}
+            />
+          </Row>
+        </Container>
 
-          {/**ABOUT SECTION */}
-          <Container fluid className = "boxShadowed">
-            <Container>
-            <Row style= {{display:"flex", justifyContent: 'center', alignItems: 'center'}}>
-              <h3 className = "homepageTitle" style = {{marginBottom:"20px"}}>
+        {/**ABOUT SECTION */}
+        <Container fluid className="boxShadowed">
+          <Container>
+            <Row
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center"
+              }}
+            >
+              <h3 className="homepageTitle" style={{ marginBottom: "20px" }}>
                 About DAML
-              </h3> 
+              </h3>
             </Row>
             <Row>
-              <Col md = {6} sm = {12} style = {{fontFamily:"Roboto Mono", display:"flex", justifyContent: 'center', alignItems: 'center'}}>
-                  Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry 's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages.
+              <Col
+                md={6}
+                sm={12}
+                style={{
+                  fontFamily: "Roboto Mono",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center"
+                }}
+              >
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry.Lorem Ipsum has been the industry 's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book. It has
+                survived not only five centuries, but also the leap into
+                electronic typesetting, remaining essentially unchanged. It was
+                popularised in the 1960s with the release of Letraset sheets
+                containing Lorem Ipsum passages.
               </Col>
-              <Col md = {6} sm = {12}>
+              <Col md={6} sm={12}>
                 <Animation />
               </Col>
             </Row>
-            </Container>
           </Container>
-
+        </Container>
       </div>
     );
   }
