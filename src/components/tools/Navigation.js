56eac67822429cae8b9b0ec6e6ca26054cdb0550
NICOLE MOISEYEV <nicole.moiseyev3400@gmail.com>
2019-10-20 22:43:53 -0400

    Restyle navbar link placement

--- src/components/tools/Navigation.js
+++ src/components/tools/Navigation.js
@@ -19,7 +19,7 @@ class Navigation extends React.Component {
       <Container
         fluid
         style={{
-          padding: "1rem",
+          padding: "0.6rem",
           marginBottom: "10px",
           backgroundColor: "#1e2c3a"
         }}
@@ -27,41 +27,27 @@ class Navigation extends React.Component {
         <Row style={{ fontSize: "20px" }}>
           {" "}
           <Col
-            xs={6}
+            xs={12}
             style={{
               alignItems: "center",
               display: "flex",
-              padding: "0.2rem 0 0 1rem"
+              padding: "0.1rem 0 0 1rem"
             }}
           >
-            <Link to="/">
+            <Link to="/" style={{ padding: "0.4rem 0 0 1rem" }}>
               <div className="daml-icon" />
             </Link>
-          </Col>
-          <Col
-            className="justify-content-end"
-            xs={6}
-            style={{
-              alignItems: "center",
-              display: "flex"
-            }}
-          >
-            <Row>
-              <Col xs={6}>
-                <div style={{ padding: "0 1rem 0 0", float: "right" }}>
-                  <Link to="/projects" className="homepage-link">
-                    Projects
-                  </Link>
-                </div>
-              </Col>
-              <Col xs={6}>
-                <div style={{ padding: "0 0 0 0" }}>
-                  <Link to="/people" className="homepage-link">
-                    People
-                  </Link>
-                </div>
-              </Col>
-            </Row>
+
+            <div style={{ padding: "0 3rem 0 4rem", float: "left" }}>
+              <Link to="/projects" className="homepage-link">
+                Projects
+              </Link>
+            </div>
+            <div style={{ padding: "0 0 0 0" }}>
+              <Link to="/people" className="homepage-link">
+                People
+              </Link>
+            </div>
           </Col>
         </Row>
       </Container>
