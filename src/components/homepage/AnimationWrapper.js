4c8935ba818e6f84188a2d39b6566ec3576aa3de
Madeline Cuozzo <mdc77@duke.edu>
2020-02-25 10:08:05 -0500

    Merging old and new

--- src/components/homepage/AnimationWrapper.js
+++ src/components/homepage/AnimationWrapper.js
@@ -1,33 +1,30 @@
-import React from 'react'
-import * as VANTA from 'vanta'
-import DOTS from 'vanta/dist/vanta.dots.min'
-import * as THREE from 'three'
+import React from "react";
+import * as VANTA from "vanta";
+import DOTS from "vanta/dist/vanta.dots.min";
+import * as THREE from "three";
 //mport BIRDS from 'vanta/dist/vanta.birds.min'
 
 // Make sure window.THREE is defined, e.g. by including three.min.js in the document head using a <script> tag
 
 class AnimationWrapper extends React.Component {
   constructor() {
-    super()
-    this.vantaRef = React.createRef()
+    super();
+    this.vantaRef = React.createRef();
   }
   componentDidMount() {
     this.vantaEffect = DOTS({
-	  el: this.vantaRef.current,
-    THREE: {...THREE},
-    showLines: false,
-    color: "#f0efe5",
-    backgroundColor: "#1e2c3a",
-    
-    })
+      el: this.vantaRef.current,
+      THREE: { ...THREE },
+      showLines: false,
+      color: "#f0efe5",
+      backgroundColor: "#1e2c3a"
+    });
   }
   componentWillUnmount() {
-    if (this.vantaEffect) this.vantaEffect.destroy()
+    if (this.vantaEffect) this.vantaEffect.destroy();
   }
   render() {
-    return <div ref={this.vantaRef}>
-      {this.props.children}
-    </div>
+    return <div ref={this.vantaRef}>{this.props.children}</div>;
   }
 }
-export default AnimationWrapper
+export default AnimationWrapper;
