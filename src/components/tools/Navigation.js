007c78d15eb672e9721baa3b01647d2fd031081b
NICOLE MOISEYEV <nicole.moiseyev3400@gmail.com>
2020-03-20 19:53:35 -0400

    I don't know if this will fix padding but it's worth a shot

--- src/components/tools/Navigation.js
+++ src/components/tools/Navigation.js
@@ -19,58 +19,54 @@ class Navigation extends React.Component {
       <Container
         fluid
         style={{
-          padding: "0",
+          padding: "0.5rem",
           backgroundColor: this.props.backgroundColor || "#f2f4f5",
           maxWidth: "100vw"
         }}
       >
-        <div style={{ padding: "0" }}>
-          <Navbar>
-            <Col xs="2" lg="1">
-              <Navbar.Brand>
-                <Link to="/">
-                  <img
-                    alt="Home"
-                    src={require("../homepage/images/DAML_Full_Grey_Transparent.svg")}
-                    width="50"
-                    height="50"
-                    className="d-inline-block align-top DAML-nav-icon"
-                  />
-                </Link>
-              </Navbar.Brand>
-            </Col>
-            <Col xs="2" lg="1">
-              <Link to="/projects" className="nav-link-col">
-                Projects
-              </Link>{" "}
-            </Col>
+        <Row>
+          <Col xs="2" lg="1">
+            <Link to="/">
+              <img
+                alt="Home"
+                src={require("../homepage/images/DAML_Full_Grey_Transparent.svg")}
+                width="50"
+                height="50"
+                className="d-inline-block align-top DAML-nav-icon"
+              />
+            </Link>
+          </Col>
+          <Col xs="2" lg="1">
+            <Link to="/projects" className="nav-link-col">
+              Projects
+            </Link>{" "}
+          </Col>
 
-            <Col xs="2" lg="1">
-              {" "}
-              <Link to="/people" className="nav-link-col">
-                People
-              </Link>
-            </Col>
+          <Col xs="2" lg="1">
+            {" "}
+            <Link to="/people" className="nav-link-col">
+              People
+            </Link>
+          </Col>
 
-            <Col xs="2" lg="1">
-              <Link to="/courses" className="nav-link-col">
-                Courses
-              </Link>
-            </Col>
+          <Col xs="2" lg="1">
+            <Link to="/courses" className="nav-link-col">
+              Courses
+            </Link>
+          </Col>
 
-            <Col xs="2" lg="1">
-              <Link to="/papers" className="nav-link-col">
-                Papers
-              </Link>
-            </Col>
+          <Col xs="2" lg="1">
+            <Link to="/papers" className="nav-link-col">
+              Papers
+            </Link>
+          </Col>
 
-            <Col xs="2" lg="1">
-              <Link to="/GPU" className="nav-link-col">
-                GPU Usage
-              </Link>
-            </Col>
-          </Navbar>
-        </div>
+          <Col xs="2" lg="1">
+            <Link to="/GPU" className="nav-link-col">
+              GPU Usage
+            </Link>
+          </Col>
+        </Row>
       </Container>
     );
   }
