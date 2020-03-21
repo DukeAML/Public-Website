9e14d6d9a27429f74676f6f6ef542a2992850c24
Madeline Cuozzo <mdc77@duke.edu>
2020-03-20 21:00:38 -0500

    GPU usage to GPUs

--- src/components/tools/Navigation.js
+++ src/components/tools/Navigation.js
@@ -63,7 +63,7 @@ class Navigation extends React.Component {
 
           <Col xs="2" lg="1">
             <Link to="/GPU" className="nav-link-col">
-              GPU Usage
+              GPUs
             </Link>
           </Col>
         </Row>
