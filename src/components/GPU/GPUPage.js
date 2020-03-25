4db3f360e4d3d817f0aedd69b24f06f20120f6f1
NICOLE MOISEYEV <nicole.moiseyev3400@gmail.com>
2020-03-25 11:40:21 -0400

    Make initial GPU page layout

--- src/components/GPU/GPUPage.js
+++ src/components/GPU/GPUPage.js
@@ -29,34 +29,55 @@ class GPUPage extends React.Component {
     // dynamically determine left and right padding around projects grid
     if (window >= 992) {
       // lg or xl
-      padding = 5;
+      padding = 10;
     } else if (window >= 768) {
       // m
-      padding = 10;
+      padding = 5;
     } else if (window >= 576) {
       // s
-      padding = 10;
+      padding = 2;
     } else {
       // xs
-      padding = 10;
+      padding = 1;
     }
-
     return (
       <div>
         <Navigation />
         <Container fluid style={{ padding: 0 }}>
-          <Container style={{ minHeight: "80vh", padding: `0 ${padding}%` }}>
+          <Container style={{ minHeight: "85vh", padding: `0 ${padding}%` }}>
             <center>
               <div className="title"> GPUs </div>
             </center>
-            <Row
-              style={{
-                display: "flex",
-                justifyContent: "center"
-              }}
-            >
-              Under construction
-            </Row>
+            <div className="gpu-header">
+              What is a GPU? <hr /> <br />
+            </div>
+            <p>
+              {" "}
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+
+            <br />
+            <div className="gpu-header">
+              Clusters and GPUs at Duke <hr />
+              <br />
+            </div>
+
+            <p>
+              {" "}
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
           </Container>
           <Footer style={{ margin: "2rem 0 0 0" }} />
         </Container>
