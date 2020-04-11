a82aa058787de220c660275e31591583f53b16b6
NICOLE MOISEYEV <nicole.moiseyev3400@gmail.com>
2020-04-11 12:25:55 -0400

    Integrate gpu page with site style

--- src/components/GPU/GPUPage.js
+++ src/components/GPU/GPUPage.js
@@ -1,13 +1,17 @@
-import './GPUPage.css';
-import React from 'react';
-import { Container, Tab } from 'semantic-ui-react';
-import GPUInfo from './components/GPUInfo';
-import StatisticsPool from './model/StatisticPool';
-import GPUChart from './components/GPUChart';
-import withWindowDimensions from '../people/withWindowDimensions';
+import "./GPUPage.css";
+import React from "react";
+
+import Navigation from "../tools/Navigation";
+import Footer from "../tools/Footer";
+import { Spinner } from "react-bootstrap";
+import { Container, Tab } from "semantic-ui-react";
+import GPUInfo from "./components/GPUInfo";
+import StatisticsPool from "./model/StatisticPool";
+import GPUChart from "./components/GPUChart";
+import withWindowDimensions from "../people/withWindowDimensions";
 
 class GPUPage extends React.Component {
-  state = {panes: []};
+  state = { panes: [], loadingClusters: true };
 
   componentDidMount() {
     this.updatePanes();
@@ -19,26 +23,62 @@ class GPUPage extends React.Component {
       return {
         menuItem: cluster,
         render: () => (
-            <Tab.Pane attached={false}>
-              <GPUChart cluster={cluster} statistics={["mem_free", "mem_used"]} />
-            </Tab.Pane>
+          <Tab.Pane attached={false}>
+            <GPUChart cluster={cluster} statistics={["mem_free", "mem_used"]} />
+          </Tab.Pane>
         )
-      }
+      };
     });
-    this.setState({panes: panes});
+    this.setState({ panes: panes, loadingClusters: false });
   }
 
   render() {
+    let window = this.props.windowWidth;
+    let padding;
+
+    // dynamically determine left and right padding around projects grid
+    if (window >= 992) {
+      // lg or xl
+      padding = 5;
+    } else if (window >= 768) {
+      // m
+      padding = 4;
+    } else if (window >= 576) {
+      // s
+      padding = 2;
+    } else {
+      // xs
+      padding = 1;
+    }
+
     return (
-        <Container>
-          <div className="main-content">
-            <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css" />
-            <GPUInfo />
-            <div style={{ height: 2 + "rem" }}></div>
-            <h3>Clusters and GPUs at Duke</h3>
-            <Tab menu={{ secondary: true, className: 'wrapped' }} panes={this.state.panes} />
+      <div>
+        <Navigation />
+        <Container fluid style={{ padding: 0, minHeight: "95vh" }}>
+          <Container style={{ padding: `0 ${padding}%` }}>
+            <div style={{ marginTop: "1rem" }}>
+              <GPUInfo />
+            </div>
+          </Container>
+          <div style={{ padding: "3rem 15%" }}>
+            <center>
+              {this.state.loadingClusters ? (
+                <div style={{ fontSize: "2rem", padding: "3rem" }}>
+                  <Spinner animation="grow" size="lg" /> <br />
+                  <br />
+                  Loading clusters
+                </div>
+              ) : (
+                <Tab
+                  menu={{ secondary: true, className: "wrapped" }}
+                  panes={this.state.panes}
+                />
+              )}
+            </center>
           </div>
         </Container>
+        <Footer />
+      </div>
     );
   }
 }
--- src/components/GPU/components/GPUInfo.js
+++ src/components/GPU/components/GPUInfo.js
@@ -1,30 +1,44 @@
-import React from 'react';
+import React from "react";
+import { Container, Row } from "react-bootstrap";
 
-const GPUInfo = props => {
+class GPUInfo extends React.Component {
+  render() {
     return (
-        <div style={{ width: "75%" }}>
-            <h1>What is a GPU?</h1>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-                occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-                mollit anim id est laborum.
-            </p>
-            <h2>Why GPU?</h2>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-                occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-                mollit anim id est laborum.
-            </p>
+      <div>
+        <center>
+          <div className="title"> GPUs </div>
+        </center>
+        <div className="gpu-header">
+          What is a GPU? <hr /> <br />
         </div>
+        <p>
+          {" "}
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        <br />
+        <div className="gpu-header">
+          Clusters and GPUs at Duke <hr />
+          <br />
+        </div>
+        <p>
+          {" "}
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </div>
     );
-};
+  }
+}
 
 export default GPUInfo;
