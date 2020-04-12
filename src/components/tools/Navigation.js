69f7cbb41861eae4766656e2377e4ee68267b0fb
NICOLE MOISEYEV <nicole.moiseyev3400@gmail.com>
2020-04-11 23:18:30 -0400

    Center nav bar

--- src/components/tools/Navigation.js
+++ src/components/tools/Navigation.js
@@ -12,76 +12,96 @@ import {
   Image
 } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import withWindowDimensions from "../people/withWindowDimensions";
 
 class Navigation extends React.Component {
   render() {
     // need to use inline styles for veertical alignment of columns so that defaults are overridden
+    let window = this.props.windowWidth;
+    let padding;
+
+    // dynamically determine left and right padding around projects grid
+
+    if (window >= 1200) {
+      //xl
+      padding = 30;
+    } else if (window >= 992) {
+      // lg
+      padding = 25;
+    } else if (window >= 768) {
+      // m
+      padding = 10;
+    } else if (window >= 576) {
+      // s
+      padding = 2;
+    } else {
+      // xs
+      padding = 1;
+    }
+
     const verticalAlign = {
       display: "flex",
-      justifyContent: "center",
-      alignItems: "center"
+      alignItems: "center",
+      justifyContent: "center"
     };
     return (
-      <Container
-        fluid
-        style={{
-          margin: 0,
-          padding: 0,
-          paddingTop: "0.5rem",
-          backgroundColor: this.props.backgroundColor || "#f2f4f5",
-          maxWidth: "100vw"
-        }}
-      >
-        <Row style={{ padding: 0, margin: 0 }}>
-          <Col xs="2" lg="1" style={verticalAlign}>
-            <Link to="/">
-              <img
-                alt="Home"
-                src={require("../homepage/images/DAML_Full_Grey_Transparent.svg")}
-                width="50"
-                height="50"
-              />
-            </Link>
-          </Col>
-          <Col xs="2" lg="1" style={verticalAlign}>
-            <Link to="/projects" className="nav-link-col">
-              Projects
-            </Link>{" "}
-          </Col>
+      <center>
+        <Container
+          fluid
+          style={{
+            margin: 0,
+            padding: `0 ${padding}%`,
+            paddingTop: "0.5rem",
+            backgroundColor: this.props.backgroundColor || "#f2f4f5",
+            maxWidth: "100vw"
+          }}
+        >
+          <Row style={{ padding: 0, margin: 0 }}>
+            <Col xs="2" style={verticalAlign}>
+              <Link to="/">
+                <img
+                  alt="Home"
+                  src={require("../homepage/images/DAML_Full_Grey_Transparent.svg")}
+                  width="50"
+                  height="50"
+                />
+              </Link>
+            </Col>
+            <Col xs="2" style={verticalAlign}>
+              <Link to="/projects" className="nav-link-col">
+                Projects
+              </Link>{" "}
+            </Col>
 
-          <Col xs="2" lg="1" style={verticalAlign}>
-            {" "}
-            <Link to="/people" className="nav-link-col">
-              People
-            </Link>
-          </Col>
+            <Col xs="2" style={verticalAlign}>
+              {" "}
+              <Link to="/people" className="nav-link-col">
+                People
+              </Link>
+            </Col>
 
-          <Col xs="2" lg="1" style={verticalAlign}>
-            <Link to="/courses" className="nav-link-col">
-              Courses
-            </Link>
-          </Col>
+            <Col xs="2" style={verticalAlign}>
+              <Link to="/courses" className="nav-link-col">
+                Courses
+              </Link>
+            </Col>
 
-          <Col xs="2" lg="1" style={verticalAlign}>
-            <Link to="/papers" className="nav-link-col">
-              Papers
-            </Link>
-          </Col>
+            <Col xs="2" style={verticalAlign}>
+              <Link to="/papers" className="nav-link-col">
+                Papers
+              </Link>
+            </Col>
 
-          
-          <Col xs="2" lg="1" style={verticalAlign}>
-
-            <Link to="/GPU" className="nav-link-col">
-              GPUs
-            </Link>
-          </Col>
-          
-  
+            <Col xs="2" style={verticalAlign}>
+              <Link to="/GPU" className="nav-link-col">
+                GPUs
+              </Link>
+            </Col>
           </Row>
-        
-      </Container>
+        </Container>
+      </center>
     );
   }
 }
 
-export default Navigation;
+export default withWindowDimensions(Navigation);
