b297866bca201855c233979f969883d2fb1c840a
NICOLE MOISEYEV <nicole.moiseyev3400@gmail.com>
2020-04-24 12:20:09 -0400

    Add loading animation to news section

--- src/components/homepage/Carousel.js
+++ src/components/homepage/Carousel.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Carousel, Row, Container, Col } from "react-bootstrap";
+import { Carousel, Row, Container, Col, Spinner } from "react-bootstrap";
 import { getNews } from "../../api/api";
 import "./HomePage.scss";
 import { Link } from "react-router-dom";
@@ -7,12 +7,12 @@ import { Link } from "react-router-dom";
 const DtcLogo = require("./dtc-logo-tag.png");
 
 class CustomCarousel extends React.Component {
-  state = { index: 0, news: [] };
+  state = { index: 0, news: [], loading: true };
 
   componentDidMount = async () => {
     const news = await getNews();
-    this.setState({ news: news });
-    console.log(this.state);
+
+    this.setState({ news: news, loading: false });
   };
 
   handleSelect = (selectedIndex, e) => {
@@ -36,15 +36,26 @@ class CustomCarousel extends React.Component {
     ));
 
     return (
-      <Container fluid style={{ width: "100%", justifyContent: "center" }}>
+      <Container
+        fluid
+        style={{
+          width: "100%",
+          justifyContent: "center",
+          alignItems: "middle"
+        }}
+      >
         <center>
-          <Carousel
-            activeIndex={this.state.index}
-            onSelect={this.handleSelect}
-            style={{ alignItems: "top" }}
-          >
-            {slides}
-          </Carousel>
+          {this.state.loading ? (
+            <Spinner animation="grow" size="lg" style={{ margin: "2rem" }} />
+          ) : (
+            <Carousel
+              activeIndex={this.state.index}
+              onSelect={this.handleSelect}
+              style={{ alignItems: "top" }}
+            >
+              {slides}
+            </Carousel>
+          )}
         </center>
       </Container>
     );
