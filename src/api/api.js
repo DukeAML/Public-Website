716ebbe593261423cd5cd319ff671916ef84a71b
Louis Hu <louis.hu@duke.edu>
2021-01-20 15:57:13 -0500

    changed corresponding vars peoplepage.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,50 +1,50 @@
-import axios from "axios";
+import axios from 'axios';
 
 const client = axios.create({
-  baseURL: "https://dukeappml.herokuapp.com",
+	baseURL: 'https://dukeappml.herokuapp.com',
 });
 
-const Airtable = require("airtable");
+const Airtable = require('airtable');
 Airtable.configure({
-  endpointUrl: "https://api.airtable.com",
-  apiKey: "", // Remove to push to git
+	endpointUrl: 'https://api.airtable.com',
+	apiKey: 'keyF6lKZWNkqR9j3I', // Remove to push to git
 });
-var base = Airtable.base("app7oThcZBwecbqqS");
+var base = Airtable.base('app7oThcZBwecbqqS');
 
 export const getProjects = async () => {
-  const { data } = await client.get("/projects");
-  console.log(data);
-  return data;
+	const { data } = await client.get('/projects');
+	console.log(data);
+	return data;
 };
 
 export const getMembers = async () => {
-  // Old CMS Integration
-  // const { data } = await client.get("/users");
-  // console.log(data);
-  // return data; 
+	// Old CMS Integration
+	// const { data } = await client.get("/users");
+	// console.log(data);
+	// return data;
 
-  let members = [];
+	let members = [];
 
-  const records = await base("Full Roster")
-    .select({ view: "Roster [Internal]" })
-    .all();
-  records.map((record) => {
-    members.push(record._rawJson.fields);
-  });
+	const records = await base('Full Roster')
+		.select({ view: 'Roster [Internal]' })
+		.all();
+	records.map((record) => {
+		members.push(record._rawJson.fields);
+	});
 
-  return members;
+	return members;
 };
 
 export const getPapers = async () => {
-  const { data } = await client.get("./papers");
-  console.log(data);
-  return data;
+	const { data } = await client.get('./papers');
+	console.log(data);
+	return data;
 };
 
 export const getNews = async () => {
-  const { data } = await client.get("./newsentries");
-  console.log(data);
-  return data;
+	const { data } = await client.get('./newsentries');
+	console.log(data);
+	return data;
 };
 
 export default { getProjects, getPapers, getNews, getMembers };
--- src/components/people/PeoplePage.js
+++ src/components/people/PeoplePage.js
@@ -1,165 +1,168 @@
-import React from "react";
+import React from 'react';
 import {
-  Container,
-  Jumbotron,
-  Button,
-  Row,
-  Col,
-  Image,
-  Card,
-  Carousel,
-  Accordion,
-  Collapse,
-  Spinner
-} from "react-bootstrap";
-import { Link, Redirect } from "react-router-dom";
+	Container,
+	Jumbotron,
+	Button,
+	Row,
+	Col,
+	Image,
+	Card,
+	Carousel,
+	Accordion,
+	Collapse,
+	Spinner,
+} from 'react-bootstrap';
+import { Link, Redirect } from 'react-router-dom';
 
-import Footer from "../tools/Footer";
-import Navigation from "../tools/Navigation";
-import withWindowDimensions from "./withWindowDimensions";
-import { getMembers } from "../../api/api";
-import { getMemberData } from "./PeopleData";
+import Footer from '../tools/Footer';
+import Navigation from '../tools/Navigation';
+import withWindowDimensions from './withWindowDimensions';
+import { getMembers } from '../../api/api';
+import { getMemberData } from './PeopleData';
 
-import Details from "./Details";
-import Person from "./Person";
-import PeopleRow from "./PeopleRow";
+import Details from './Details';
+import Person from './Person';
+import PeopleRow from './PeopleRow';
 //import { CRM, DS, PS } from "./PeopleData";
 
 //const peopleInfo = [CRM, DS, PS];
 
 class PeoplePage extends React.Component {
-  state = {
-    loading: true,
-    details: {},
-    collapse: false,
-    members: { EXEC: [], CRM: [], DS: [], PS: [] }
-  };
+	state = {
+		loading: true,
+		details: {},
+		collapse: false,
+		members: { EXEC: [], CRM: [], DS: [], PS: [] },
+	};
 
-  componentDidMount = async () => {
-    const members = await getMemberData();
-    console.log(members);
-    this.setState({ loading: false, members: members });
-  };
+	componentDidMount = async () => {
+		const members = await getMemberData();
+		console.log(members);
+		this.setState({ loading: false, members: members });
+	};
 
-  makePeopleGrid(people, window) {
-    // determine number of people per row based on bootstrap screen breakpoints
-    let cols;
-    if (window >= 992) {
-      // lg or xl; 4 people per row
-      cols = 4;
-    } else if (window >= 768) {
-      // m; 4 people per row
-      cols = 3;
-    } else if (window >= 576) {
-      // xs; 2 people per row
-      cols = 2;
-    } else {
-      //xs; 1 person per row
-      cols = 1;
-    }
+	makePeopleGrid(people, window) {
+		// determine number of people per row based on bootstrap screen breakpoints
+		let cols;
+		if (window >= 992) {
+			// lg or xl; 4 people per row
+			cols = 4;
+		} else if (window >= 768) {
+			// m; 4 people per row
+			cols = 3;
+		} else if (window >= 576) {
+			// xs; 2 people per row
+			cols = 2;
+		} else {
+			//xs; 1 person per row
+			cols = 1;
+		}
 
-    console.log("Cols: ", cols);
-    console.log(people.length);
+		console.log('Cols: ', cols);
+		console.log(people.length);
 
-    const numRows = Math.ceil(people.length / cols);
-    console.log("rows: ", numRows);
+		const numRows = Math.ceil(people.length / cols);
+		console.log('rows: ', numRows);
 
-    let rowArrays = [];
-    // make each row, add details section below
+		let rowArrays = [];
+		// make each row, add details section below
 
-    for (let i = 0; i < numRows * cols; i += cols) {
-      rowArrays[i] = people.slice(i, i + cols);
-    }
+		for (let i = 0; i < numRows * cols; i += cols) {
+			rowArrays[i] = people.slice(i, i + cols);
+		}
 
-    let result = rowArrays.map((row, index) => (
-      <PeopleRow people={row} key={index} />
-    ));
+		let result = rowArrays.map((row, index) => (
+			<PeopleRow people={row} key={index} />
+		));
 
-    return this.state.loading ? (
-      <div style={{ height: "10rem", padding: "3rem" }}>
-        <Spinner animation="grow" size="md" />
-      </div>
-    ) : (
-      result
-    );
-  }
+		return this.state.loading ? (
+			<div style={{ height: '10rem', padding: '3rem' }}>
+				<Spinner animation='grow' size='md' />
+			</div>
+		) : (
+			result
+		);
+	}
 
-  render() {
-    const { EXEC, CRM, DS, PS } = this.state.members;
+	render() {
+		const { EXEC, PM, DS, SWE, HRD } = this.state.members;
 
-    let CRMgrid = this.makePeopleGrid(CRM, this.props.windowWidth);
-    let DSgrid = this.makePeopleGrid(DS, this.props.windowWidth);
-    let PSgrid = this.makePeopleGrid(PS, this.props.windowWidth);
-    let EXECgrid = this.makePeopleGrid(EXEC, this.props.windowWidth);
-    let window = this.props.windowWidth;
-    let padding;
+		let EXECgrid = this.makePeopleGrid(EXEC, this.props.windowWidth);
+		let PMgrid = this.makePeopleGrid(PM, this.props.windowWidth);
+		let DSgrid = this.makePeopleGrid(DS, this.props.windowWidth);
+		let SWEgrid = this.makePeopleGrid(SWE, this.props.windowWidth);
+		let HRDgrid = this.makePeopleGrid(HRD, this.props.windowWidth);
+		let window = this.props.windowWidth;
+		let padding;
 
-    // dynamically determine left and right padding around people grid
-    if (window >= 992) {
-      // lg or xl
-      padding = 20;
-    } else if (window >= 768) {
-      // m
-      padding = 5;
-    } else if (window >= 576) {
-      // s
-      padding = 10;
-    } else {
-      // xs
-      padding = 5;
-    }
+		// dynamically determine left and right padding around people grid
+		if (window >= 992) {
+			// lg or xl
+			padding = 20;
+		} else if (window >= 768) {
+			// m
+			padding = 5;
+		} else if (window >= 576) {
+			// s
+			padding = 10;
+		} else {
+			// xs
+			padding = 5;
+		}
 
-    // center titles on mobile
+		// center titles on mobile
 
-    let EXECtitle, DStitle, PStitle, CRMtitle;
+		let EXECtitle, PMtitle, DStitle, SWEtitle, HRDtitle;
 
-    if (window <= 576) {
-      EXECtitle = <center> Leadership Team </center>;
-      DStitle = <center> Data Science Team </center>;
-      PStitle = <center> Implementation Team </center>;
-      CRMtitle = <center> Business Team </center>;
-    } else {
-      EXECtitle = "Leadership Team";
-      DStitle = "Data Science Team";
-      PStitle = "Implementation Team";
-      CRMtitle = "Business Team";
-    }
+		if (window <= 576) {
+			EXECtitle = <center> Executive Team </center>;
+			PMtitle = <center> Product Manager Team</center>;
+			DStitle = <center> Data Science Team </center>;
+			SWEtitle = <center> Software Engineer Team </center>;
+			HRDtitle = <center> Hardware Engineer Team </center>;
+		} else {
+			EXECtitle = 'Executive Team';
+			PMtitle = 'Product Manager Team';
+			DStitle = 'Data Science Team';
+			SWEtitle = 'Software Engineer Team';
+			HRDtitle = 'Hardware Engineer Team';
+		}
 
-    return (
-      <Container fluid style={{ padding: 0 }}>
-        <Navigation />
+		return (
+			<Container fluid style={{ padding: 0 }}>
+				<Navigation />
 
-        <div style={{ minHeight: "85vh" }}>
-          <div className="title">
-            <center>Our Members</center>
-          </div>
-          <div className="team" style={{ padding: `1rem ${padding}%` }}>
-            {EXECtitle}
-            <hr />
-          </div>
-          <center>{EXECgrid}</center>
-          <div className="team" style={{ padding: `1rem ${padding}%` }}>
-            {DStitle}
-            <hr />
-          </div>
-          <center>{DSgrid}</center>
-          <div className="team" style={{ padding: `1rem ${padding}%` }}>
-            {PStitle}
-            <hr />
-          </div>
-          <center>{PSgrid}</center>
+				<div style={{ minHeight: '85vh' }}>
+					<div className='title'>
+						<center>Our Members</center>
+					</div>
+					<div className='team' style={{ padding: `1rem ${padding}%` }}>
+						{EXECtitle}
+						<hr />
+					</div>
+					<center>{EXECgrid}</center>
+					<div className='team' style={{ padding: `1rem ${padding}%` }}>
+						{DStitle}
+						<hr />
+					</div>
+					<center>{DSgrid}</center>
+					<div className='team' style={{ padding: `1rem ${padding}%` }}>
+						{PStitle}
+						<hr />
+					</div>
+					<center>{PSgrid}</center>
 
-          <div className="team" style={{ padding: `1rem ${padding}%` }}>
-            {CRMtitle}
-            <hr />
-          </div>
-          <center>{CRMgrid}</center>
-        </div>
+					<div className='team' style={{ padding: `1rem ${padding}%` }}>
+						{CRMtitle}
+						<hr />
+					</div>
+					<center>{CRMgrid}</center>
+				</div>
 
-        <Footer />
-      </Container>
-    );
-  }
+				<Footer />
+			</Container>
+		);
+	}
 }
 
 export default withWindowDimensions(PeoplePage);
